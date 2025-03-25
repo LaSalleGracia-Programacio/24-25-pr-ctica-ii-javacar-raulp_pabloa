ccf8da44b128ec4945aa68efbe3b015035d48e8a
Pablo <pablo.arenas@gracia.lasalle.cat>
2025-03-25 20:18:20 +0100

    Creacion de la clase GestorHistorial donde podremos guardar y consultar los alquileres

---
+++ src/main/java/org/JavaCar/GestorHistorial.java
@@ -0,0 +1,12 @@
+package org.JavaCar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GestorHistorial {
+    private List<Lloguer> historial;
+
+    public GestorHistorial() {
+        this.historial = new ArrayList<>();
+    }
+}
