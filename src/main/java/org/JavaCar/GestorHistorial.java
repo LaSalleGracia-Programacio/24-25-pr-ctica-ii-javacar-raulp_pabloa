4b8befc924ce73b645adc7a9b4e7d14148300449
Pablo <pablo.arenas@gracia.lasalle.cat>
2025-03-25 20:19:02 +0100

    Añadido de mas metodos en la clase gestor historial

--- src/main/java/org/JavaCar/GestorHistorial.java
+++ src/main/java/org/JavaCar/GestorHistorial.java
@@ -9,4 +9,19 @@ public class GestorHistorial {
     public GestorHistorial() {
         this.historial = new ArrayList<>();
     }
+    // Registrar un nuevo alquiler
+    public void registrarLloguer(Lloguer lloguer) {
+        historial.add(lloguer);
+    }
+
+    // Consultar historial de alquileres por cliente
+    public List<Lloguer> consultarHistorialPorCliente(String clientId) {
+        List<Lloguer> resultado = new ArrayList<>();
+        for (Lloguer lloguer : historial) {
+            if (lloguer.getClientId().equals(clientId)) {
+                resultado.add(lloguer);
+            }
+        }
+        return resultado;
+    }
 }
